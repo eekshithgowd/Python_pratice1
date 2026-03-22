afe17f28ee556f4ac0b6e328dcde00621ff7f6b4
eekshith gowd <eekshithgowd@gmail.com>
2026-03-22 13:08:24 +0530

    Updating shell script file

--- gitpython/git_dynamic_message_commit.sh
+++ gitpython/git_dynamic_message_commit.sh
@@ -3,6 +3,11 @@
 echo "Enter commit message:"
 read msg
 
+echo "Adding changes..."
 git add .
+echo "Committing changes..."
 git commit -m "$msg"
+echo "Pushing to GitHub..."
 git push
+
+echo "Done ✅"
